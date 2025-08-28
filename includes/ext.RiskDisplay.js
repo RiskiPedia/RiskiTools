c0802c00b1d7433975e5dc217ad75aea64f07f08
Gavin Andresen <gavinandresen@gmail.com>
2025-08-28 15:39:06 -0600

    Treat RiskModel display text as wikitext
    This was easier than I expected! Riskmodel text is
    now sent to the server to be parsed, so any wikitext tags
    (like using {{Templates}} or other wikitext fanciness) are
    expanded and displayed properly.

--- includes/ext.RiskDisplay.js
+++ includes/ext.RiskDisplay.js
@@ -12,7 +12,29 @@ mw.loader.using(['oojs-ui'], function () {
             try {
                 const result = eval(jscode); // jscode is server-generated, so we know it's safe
                 const updatedText = originalText.replace(/{result}/g, result);
-                e.text(updatedText);
+
+                // Send the text to the server to parse (surrounded by a unique string
+                // because we want to strip out the extraneous div's and p's the server
+                // wraps it in)
+                const uniquetext = 'z3IP5fEV3B9qSE';
+                const wikitext = uniquetext+updatedText+uniquetext;
+                var api = new mw.Api();
+                api.get( {
+                    action: 'parse',
+                    format: 'json',
+                    formatversion: 2,
+                    contentmodel: 'wikitext',
+                    text: wikitext,
+                    prop: 'text'
+                } ).then( ( data ) => {
+                    const r = data.parse.text;
+                    const startIndex = r.indexOf(uniquetext);
+                    const endIndex = r.lastIndexOf(uniquetext);
+                    e.text(r.substring(startIndex + uniquetext.length, endIndex));
+                } ).catch((error) => {
+                    e.text('Error: Unable to update risk display');
+                    console.error('API request failed:', error);
+                });
             } catch (error) {
                 e.text('');
             }
